1c4036ad5fb9d1e4ffcdfc91aec2db830ba49e0f
Jack Battye <66002339+JackBattye91@users.noreply.github.com>
2025-03-18 14:38:04 +0000

    Added API project and Common Lib

---
+++ SnipIt.Common/Enums/Languages.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnipIt.Common.Enums
+{
+    public enum Languages
+    {
+        None,
+        Bicep,
+        C,
+        CPP,
+        CSharp,
+        CSS,
+        HTML,
+        Java,
+        Javascript,
+        Other
+    }
+}
---
+++ SnipIt.Common/Models/Snippet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SnipIt.Common.Enums;
+
+namespace SnipIt.Common.Models
+{
+    public class Snippet
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public Languages Language { get; set; } = Languages.None;
+        public string Code { get; set; } = string.Empty;
+        public List<string> Tags { get; set; } = new List<string>();
+    }
+}
